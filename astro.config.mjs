b7c8eb3ef205e00a7549b21114999e748256ecb3
Amalia Kamien <ak5383@columbia.edu>
2025-04-30 18:28:16 -0400

    Connect cleaned history to working site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://design-practices.github.io",
+  site: "https://amaltal.github.io/transition",
   base: "astro-maplibre-template",
   integrations: [mdx(), sitemap(), tailwind()],
 });
